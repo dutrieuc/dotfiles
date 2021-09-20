6b966a3c79d3fb8b54c2ebd865b258b780792613
Cyril Dutrieux (cydu) <cydu@odoo.com>
2021-09-20 19:21:43 +0200

    Move hidden files

--- pkg_install.sh
+++ pkg_install.sh
@@ -1,3 +1,4 @@
-sudo pacman -Syu alacritty ripgrep fd bat ttf-meslo-nerd-font-powerlevel10k crystal shards jq
+sudo pacman -Syu alacritty ripgrep fd bat ttf-meslo-nerd-font-powerlevel10k crystal shards jq broot fzf
+broot --install
 cd ~/.dotfiles//repositories/kakoune.cr/
 make install
