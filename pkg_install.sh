5180e001bb46da5ec5c424d6ca4cd64296ce9ae1
Cyril Dutrieux (cydu) <cydu@odoo.com>
2021-10-02 11:02:57 +0200

    Enable space to meta, caps2esc

--- pkg_install.sh
+++ pkg_install.sh
@@ -1,5 +1,7 @@
-sudo pacman -Syu alacritty ripgrep fd bat ttf-meslo-nerd-font-powerlevel10k crystal shards jq broot fzf gnome-keyring libsecret yay
-yay -S ant-dracula-gtk-theme ant-dracula-kvantum-theme-git activitywatch-bin dracula-alacritty-git
+sudo pacman -Syu alacritty ripgrep fd bat ttf-meslo-nerd-font-powerlevel10k crystal shards jq broot fzf gnome-keyring libsecret yay caps2esc
+yay -S ant-dracula-gtk-theme ant-dracula-kvantum-theme-git activitywatch-bin dracula-alacritty-git interception-space2meta
+sudo systemclt enable udevmon.service
+sudo systemclt start udevmon.service
 broot --install
 cd ~/.dotfiles//repositories/kakoune.cr/
 make install
