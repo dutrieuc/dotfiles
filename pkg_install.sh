14093f81ae22c5d1a2ec2c8d5db03fdf72f381b5
Cyril Dutrieux (cydu) <cydu@odoo.com>
2021-10-08 13:35:02 +0200

    Add LSP config

--- pkg_install.sh
+++ pkg_install.sh
@@ -1,5 +1,7 @@
 sudo pacman -Syu alacritty ripgrep fd bat ttf-meslo-nerd-font-powerlevel10k crystal shards jq broot fzf gnome-keyring libsecret yay caps2esc
-yay -S ant-dracula-gtk-theme ant-dracula-kvantum-theme-git activitywatch-bin dracula-alacritty-git interception-space2meta
+yay -S flow lemminx ant-dracula-gtk-theme ant-dracula-kvantum-theme-git activitywatch-bin dracula-alacritty-git interception-space2meta
+pip install python-lsp-server python-lsp-black pyflakes mccabe pycodestyle rope
+
 sudo systemclt enable udevmon.service
 sudo systemclt start udevmon.service
 broot --install
