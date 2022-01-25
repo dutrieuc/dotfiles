a09f049dfc78ed73a32edce00ab7c1b69ba85907
Cyril Dutrieux (cydu) <cydu@odoo.com>
2022-01-25 18:59:04 +0100

    [master] edit odoo specific functions

--- scripts/odoo-fn.sh
+++ scripts/odoo-fn.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 export-scaffold () {
     CUR_DIR=${PWD}
-    ODOO_WS="/home/dutrieux/workspace/odoosrc"
+    # ODOO_WS="~/"
     CLIENT_WS="${PWD}/${1}"
     mkdir ${CLIENT_WS}
-    cd ${ODOO_WS}/tools/export-tools/
+    cd ${HOME}/tools-odoo/export/
     python3 export.py scaffold ${CLIENT_WS} $2 $3
     cd $CUR_DIR
 }
 
 odrr () {
-  odev "$@" --dev=xml,reload --log-handler odoo.addons.base.models.ir_attachment:WARNING
+  odev "$@" --dev=xml --log-handler odoo.addons.base.models.ir_attachment:WARNING
 }
 
 odr () {
